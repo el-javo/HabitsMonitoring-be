f5f269e2333bccdc600d325e4369351652966657
el-javo <javimuu99@gmail.com>
2024-01-10 19:50:37 +0100

    feat: now return habitDays w/ habit object and implement habit days updating

--- src/controllers/habitController.js
+++ src/controllers/habitController.js
@@ -1,6 +1,47 @@
 const BaseController = require("@controllers/baseController");
 const HabitRepository = require("@repositories/habitRepository");
+const WeekDayHabitRepository = require("@repositories/weekDayHabitRepository");
 
-class HabitController extends BaseController {}
+class HabitController extends BaseController {
+  create = async (req, res, next) => {
+    const body = req.body;
+    if (!body || !body.weekDays || !body.weekDays.length) {
+      return res.status(400).json({
+        error:
+          !body.weekDays && body ? "bad request (weekDays)" : "bad request",
+      });
+    }
+    const weekDays = body.weekDays;
+    delete body.weekDays;
+    const newEntity = await this.repository.create(body);
+    const habitId = newEntity.id;
+    for (let weekDayId of weekDays) {
+      await WeekDayHabitRepository.create({ habitId, weekDayId });
+    }
+    const newHabit = await this.repository.findBy({ id: habitId });
+    return res.json({ result: newHabit });
+  };
+
+  update = async (req, res, next) => {
+    const id = req.params.id;
+    const body = req.body;
+    const { weekDays } = body;
+    delete body.weekDays;
+    if (!body || !id) {
+      return res.status(400).json({
+        error: "bad request",
+      });
+    }
+    await this.repository.update({ id }, body);
+    if (weekDays && weekDays.length) {
+      await WeekDayHabitRepository.delete({ habitId: id });
+      for (let weekDayId of weekDays) {
+        await WeekDayHabitRepository.create({ habitId: id, weekDayId });
+      }
+    }
+    const newHabit = await this.repository.findBy({ id });
+    return res.json({ result: newHabit });
+  };
+}
 
 module.exports = new HabitController(HabitRepository);
--- src/repositories/habitRepository.js
+++ src/repositories/habitRepository.js
@@ -1,6 +1,18 @@
 const BaseRepository = require("@repositories/baseRepository");
 const habitModel = require("@models/habitModel");
+const WeekDayHabitRepository = require("@repositories/weekDayHabitRepository");
 
-class HabitRepository extends BaseRepository {}
+class HabitRepository extends BaseRepository {
+  findBy = async (query) => {
+    const habits = await this.model.findAll({ where: query });
+    return await Promise.all(
+      habits.map(async (h) => {
+        const weekDays = await WeekDayHabitRepository.findBy({ habitId: h.id });
+        h.dataValues.weekDays = weekDays.map((w) => w.weekDayId);
+        return h;
+      })
+    );
+  };
+}
 
 module.exports = new HabitRepository(habitModel);
